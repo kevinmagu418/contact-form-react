fd4be92946756c6663029171815a131f95ce0479
kevinmagu418 <kevomagunas439@gmail.com>
2024-07-06 16:01:16 +0300

    contactme form

---
+++ form/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Contactus from './components/Contactus'
+function App() {
+
+
+  return (
+    <>
+   <div className='parentcontainer'>
+    <Contactus/>
+</div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ form/src/components/Contactus.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+
+export default function Contactus() {
+    //javascript
+    const [selectedCheckbox, setSelectedCheckbox] = useState(null);
+
+const handleChange=(event)=>{
+
+    const { name } = event.target;
+    setSelectedCheckbox(name);
+  };
+  //usestate for other form inputs
+  const [firstname, setfirstname] = useState('')
+  const [lastname, setlastname] = useState('')
+  const [email, setemail] = useState('')
+  const [message, setmessage] = useState('')
+  const [accept, setAccept] = useState(false)
+
+
+  //handle submit to the local storage and creating a variable to handle all state variables
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      firstname,
+      lastname,
+      email,
+      accept,
+      message,
+      selectedCheckbox
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
+    console.log('Form Data Saved:', formData);
+  };
+
+  
+  
+
+  return (
+    <form className='Container' method='post'>
+        {/*h2heading */}
+       <div className='h2div'>
+          <h2>Contact us</h2>
+       </div>
+       {/*name inputs */}
+       <div className='namediv'>
+           <div className='firstname'>
+           <label htmlFor="fname">FirstName*</label>
+          <input className='name' type="text"  required id='fname' min='4' placeholder='Enter your first name' value={firstname} onChange={(e)=>setfirstname(e.target.value)} />
+          </div>
+          <div className='lastname'>
+        <label htmlFor="Lname">LastName*</label>
+          <input className='name' type="text" required id='Lname' min='4' placeholder='Enter ur last name'value={lastname} onChange={(e)=>setlastname(e.target.value)}/>
+        </div>
+       </div>
+       {/*email input */}
+       <div className='email'>
+        <label htmlFor="email">EmailAdress</label>
+        <input className='emailinput' type="email" id='email' placeholder='johndoe@gmail.com' value={email} onChange={(e)=>setemail(e.target.value)} />
+       </div>
+       {/*a query checkbox divs */}
+            <div class="form-check">
+                <h6>Query Type*</h6>
+                <div className='checkme'>
+                <div className='checkenclose'>
+              <input type="checkbox" name='checkbox1' className='checkbox1' checked={selectedCheckbox==='checkbox1'} onChange={handleChange}/>
+              <label>General Enquiry</label>
+              </div> 
+              <div className='checkenclose'>
+              <input type="checkbox" name='checkbox2' className='checkbox2' checked={selectedCheckbox==='checkbox2'}onChange={handleChange} />
+              <label>Support Request</label>
+              </div>
+              </div> 
+            </div>
+            {/*textbox that acccepts user suggestions */}
+                <div className='message'>
+                    <label htmlFor="message"className='messagelabel' >Message*</label>
+                    <textarea name="message" id="message" value={message} onChange={(e)=>setmessage(e.target.value)} ></textarea>
+                </div>
+                {/*terms and conditions checkbox */}
+                <div className='termsandconditions'>
+                    <input type="checkbox"checked={accept}  onChange={(e)=>setAccept(e.target.checked)}/>
+                    <label htmlFor="terms" className='consent'>I consent to being contacted by the team*</label>
+                </div>
+                {/*a submit button to submit the contact form */}
+                <div className='submitbtn'>
+                    <button type='submit'onClick ={handleSubmit}>Submit</button>
+                </div>
+     
+    </form>
+  );
+};
